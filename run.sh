322dd3d02e3612cbd03982c0fd268e204a30bbeb
Rayi <bronya_rayi@qq.com>
2023-06-11 20:44:51 +0800

    忘了提交数据库格式了

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 nohup redis-server > /dev/null 2>&1 &
 sleep 1
-nohup python3 -m celery -A app.celery_task.celery_tasks worker --loglevel=INFO > ./log/system_status.log &
+nohup python3 -m celery -A app.celery_task.celery_tasks worker --loglevel=WARNING > ./log/system_status.log &
 sleep 2
 nohup python3 -m celery -A app.celery_task.celery_beat_task beat --loglevel=WARNING > /dev/null 2>&1 &
 python3 PDScan.py
